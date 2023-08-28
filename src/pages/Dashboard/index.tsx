9e4a6cd71fd8390a83392a39f930b0c3f87d5901
lucas <lucas327ks@gmail.com>
2023-08-28 20:32:50 -0300

    modify pages to `typescript`

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,49 @@
+import './styles.css'
+
+import { useEffect, useState } from 'react'
+import {
+	getWeatherByCity,
+	getWeatherByCityResponseProps
+} from '../../services/getWeatherByCity'
+
+import { Today } from '../../components/Today'
+import { Details } from '../../components/Details'
+import { Loading } from '../../components/Loading'
+import { NextDays } from '../../components/NextDays'
+import { CityProps } from '../../services/getCityByNameService'
+
+export function Dashboard() {
+	const [data, setData] = useState<getWeatherByCityResponseProps>(
+		{} as getWeatherByCityResponseProps
+	)
+	const [isLoading, setIsLoading] = useState(true)
+	const [city, setCity] = useState<CityProps>(
+		JSON.parse(localStorage.getItem('@typewheather:city') ?? '')
+	)
+
+	useEffect(() => {
+		setIsLoading(true)
+
+		const { latitude, longitude } = city
+
+		getWeatherByCity({ latitude, longitude })
+			.then((response) => setData(response))
+			.finally(() => setIsLoading(false))
+	}, [city])
+
+	if (isLoading) {
+		return <Loading />
+	}
+
+	return (
+		<div className="dashboard">
+			<Today
+				city={city.name}
+				onSearchValue={setCity}
+				weather={data.today.weather}
+			/>
+			<Details data={data.today.details} />
+			<NextDays data={data.nextDays} />
+		</div>
+	)
+}
